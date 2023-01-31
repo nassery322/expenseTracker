eb476c33a9dd0a5185a197d6a9adf744e4bee0f3
nassery322 <ahmadullahnassery322@gmail.com>
2023-01-31 11:52:31 +0300

    upgraded

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,84 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './ExpenseForm.css';
+import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('');
-  const [error, setError] = useState(false)
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const [error, setError] = useState(false);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
-setError(false)
+    setError(false);
   };
 
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
-setError(false)
+    setError(false);
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-setError(false)
+    setError(false);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    if(enteredAmount && enteredTitle && enteredDate){
+    if (enteredAmount && enteredTitle && enteredDate) {
       const expenseData = {
         title: enteredTitle,
         amount: enteredAmount,
         date: new Date(enteredDate),
       };
-  
-      props.onSaveExpenseData(expenseData);
-      setError(false)
-    }else{setError(true)
 
+      props.onSaveExpenseData(expenseData);
+      setError(false);
+    } else {
+      setError(true);
     }
-  
-    setEnteredTitle('');
-    setEnteredAmount('');
-    setEnteredDate('');
+
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Title</label>
           <input
-            type='text'
+            type="text"
             value={enteredTitle}
             onChange={titleChangeHandler}
           />
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Amount</label>
           <input
-            type='number'
-            min='0.01'
-            step='0.01'
+            type="number"
+            min="0.01"
+            step="0.01"
             value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Date</label>
           <input
-            type='date'
-            min='2019-01-01'
-            max='2022-12-31'
+            type="date"
+            min="2019-01-01"
+            max="2023-12-31"
             value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
       </div>
-      {error && <p className='error'>Please fill in all the fields!</p>}
-      <div className='new-expense__actions'>
-        <button type="button" onClick={props.onCancel}>Cancel</button>
-        <button type='submit'>Add Expense</button>
+      {error && <p className="error">Please fill in all the fields!</p>}
+      <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
